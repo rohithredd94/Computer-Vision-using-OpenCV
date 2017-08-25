dab434f353af15a5f6b07218cff6502f26d52721
Rohith Reddy <rohithreddyk94@gmail.com>
2017-08-25 17:41:54 -0500

    Source code for Mean Shift Line PF Tracker

---
+++ Particle-Filter-Tracking/MSL_PF_Tracker.py
@@ -0,0 +1,104 @@
+import cv2
+import numpy as np
+from hist import *
+
+class MSL_PF_Tracker:
+    def __init__(self, model, search_space, num_particles=100, state_dims=2,
+                 control_std=10, sim_std=20, alpha=0.0):
+        self.model = model
+        self.search_space = search_space[::-1]
+        self.num_particles = num_particles
+        self.state_dims = state_dims
+        self.control_std = control_std
+        self.sim_std = sim_std
+        self.alpha = alpha
+
+        #Inialize particles using a uniform distribution
+        self.particles = np.array([np.random.uniform(0, self.search_space[i],self.num_particles) for i in range(self.state_dims)]).T
+        self.weights = np.ones(len(self.particles)) / len(self.particles)
+        self.idxs = np.arange(num_particles)
+        self.estimate_state()
+
+    def update(self, frame):
+        self.displace()
+        self.observe(frame)
+        self.resample()
+        self.estimate_state()
+
+        if self.alpha > 0:
+            self.update_model(frame)
+
+    def displace(self):
+        #Displace particles using a normal distribution centered around 0
+        self.particles += np.random.normal(0, self.control_std,
+                                           self.particles.shape)
+
+    def observe(self, img):
+        # get patches corresponding to each particle
+        mh, mw = self.model.shape[:2]
+        minx = (self.particles[:,0] - mw/2).astype(np.int)
+        miny = (self.particles[:,1] - mh/2).astype(np.int)
+
+        candidates = [img[miny[i]:miny[i]+mh, minx[i]:minx[i]+mw]
+                      for i in range(self.num_particles)]
+
+        #Compute importance weight - similarity of each patch to the model
+        self.weights[:] = [hist(cand, self.model, std=10,num_bins=8) for cand in candidates]
+        self.weights /= self.weights.sum()
+
+    def resample(self):
+        sw, sh = self.search_space[:2]
+        mh, mw = self.model.shape[:2]
+        
+        distrib = self.weights.flatten() #Sample new particle indices using the distribution of the weights
+
+        j = np.random.choice(self.idxs, self.num_particles, True, p=distrib)
+        control = np.random.normal(0, self.control_std, self.particles.shape)
+        self.particles = np.array(self.particles[j])
+
+        #Clip particles in case the window goes out of the image limits
+        self.particles[:,0] = np.clip(self.particles[:,0], 0, sw - 1)
+        self.particles[:,1] = np.clip(self.particles[:,1], 0, sh - 1)
+
+    def estimate_state(self):
+        state_idx = np.random.choice(self.idxs, 1, p=self.weights.flatten())
+        self.state = self.particles[state_idx][0]
+
+    def update_model(self, frame):
+
+        #Get current model based on belief
+        mh, mw = self.model.shape[:2]
+        minx = int(self.state[0] - mw/2)
+        miny = int(self.state[1] - mh/2)
+        best_model = frame[miny:miny+mh, minx:minx+mw]
+
+        #Apply appearance model update if new model shape is unchanged
+        if best_model.shape == self.model.shape:
+            self.model = self.alpha * best_model + (1-self.alpha) * self.model
+            self.model = self.model.astype(np.uint8)
+
+    def visualize_filter(self, img):
+        self.draw_particles(img)
+        self.draw_window(img)
+        self.draw_std(img)
+
+    def draw_particles(self, img):
+        for p in self.particles:
+            cv2.circle(img, tuple(p.astype(int)), 2, (180,255,0), -1)
+
+    def draw_window(self, img):
+        best_idx = cv2.minMaxLoc(self.weights)[3][1]
+        best_state = self.particles[best_idx]
+        pt1 = (best_state - np.array(self.model.shape[1::-1])/2).astype(np.int)
+
+        pt2 = pt1 + np.array(self.model.shape[1::-1])
+        cv2.rectangle(img, tuple(pt1), tuple(pt2), (0,255,0), 2)
+
+    def draw_std(self, img):
+        weighted_sum = 0
+        dist = np.linalg.norm(self.particles - self.state)
+        weighted_sum = np.sum(dist * self.weights.ravel())
+
+        cv2.circle(img, tuple(self.state.astype(np.int)),
+                   int(weighted_sum), (255,255,255), 1)
+
---
+++ Particle-Filter-Tracking/msltracker.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+import time
+import sys
+from MSL_PF_Tracker import *
+
+videos= ['noisy_debate','pedestrians','pres_debate']
+text = ['noisy_debate','pedestrians','pres_debate','pres_debate_hand'] #These contain the model coordinates
+
+def pf_tracker(video, text, frames_to_save=[], play_video=True, num_particles=100, dimensions=2,
+                control=10, sim_std=20, alpha=0):
+    capture = cv2.VideoCapture('./resources/'+video+'.avi')
+
+    flag, frame = capture.read()
+    if not flag:
+        print("FATAL ERROR: Unable to read first frame. Program will now exit")
+        return
+
+    search_space = np.array(frame.shape)
+
+    f = open('./resources/' + text + '.txt', 'r').read().split()
+    s = {'x': float(f[0]), 'y': float(f[1]), 'w': float(f[2]), 'h': float(f[3])} #Extracting the model co-ordinates
+    print(s)
+    miny = int(s['y']); maxy = int(miny + s['h'])
+    minx = int(s['x']); maxx = int(minx + s['w']) #Mapping the co-ordinates onto the frame
+    model = frame[miny:maxy, minx:maxx]
+    cv2.imwrite('results/msl/firstframe_imagepatch.png', frame[miny:maxy, minx:maxx])
+
+    pftracker = MSL_PF_Tracker(model, search_space, num_particles, dimensions, control, sim_std, alpha)
+
+    frame_count = 1
+    saved_frames_count = 0
+    while capture.isOpened():
+        start = time.time()
+        flag, frame = capture.read()    
+        if not flag:
+            break
+        
+        frame_count += 1
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        pftracker.update(gray) #update tracker with new info
+        pftracker.visualize_filter(frame) #Draw the new frame
+
+        if len(pftracker.model.shape) < 3:
+            color_model = cv2.cvtColor(pftracker.model, cv2.COLOR_GRAY2BGR)
+        frame[:model.shape[0], :model.shape[1]] = color_model
+
+        delay = int(25 - (time.time() - start))
+        if cv2.waitKey(delay) & 0xFF == ord('q'): #Hit 'q' key at any point to stop video
+            break
+        cv2.imshow(video, frame)
+
+        if frame_count in frames_to_save:
+            cv2.imwrite('results/msl/frame-'+str(frame_count)+'.png', frame)
+            saved_frames_count
+
+if __name__ == '__main__':
+    print("Mean Shift Line Particle Filter Tracking Started")
+    pf_tracker(videos[2], text[2], [28,84,144], play_video=True, num_particles=100, dimensions=2,
+                          control=10, sim_std=20, alpha=0) #Function to start Particle Filter Tracking
+    print("Mean Shift Line Particle Filter Tracking End")
+
+    print("Mean Shift Line Particle Filter Tracking with appearance model update Started")
+    pf_tracker(videos[2], text[3], [28,84,144], play_video=True, num_particles=700, dimensions=2,
+                          control=10, sim_std=20, alpha=0) #Function to start Particle Filter Tracking
+    print("Mean Shift Line Particle Filter Tracking with appearance model update End")
